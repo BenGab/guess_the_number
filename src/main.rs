b64d82b1313e3aebbb480ed64dd3923183aadf0b
Gabor Benko <benkogabor87@gmail.com>
2024-03-01 16:11:48 +0100

    guessing loop

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,28 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("Please input your progress");
+    loop {
+        
+        println!("Please input your progress");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number !!!");
+        let guess: u32 = guess.trim().parse().expect("Please type a number !!!");
 
-    println!("You guessed: {guess}");
+        println!("You guessed: {guess}");
 
-    match guess.cmp(&secret_number) {
-        std::cmp::Ordering::Less => println!("Too small"),
-        std::cmp::Ordering::Equal => println!("You Win"),
-        std::cmp::Ordering::Greater => println!("Too big"),
+        match guess.cmp(&secret_number) {
+            std::cmp::Ordering::Less => println!("Too small"),
+            std::cmp::Ordering::Equal => {
+                println!("You Win");
+                break;
+            },
+            std::cmp::Ordering::Greater => println!("Too big"),
+        }
     }
 
 }

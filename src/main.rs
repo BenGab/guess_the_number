38bfb71585819a42bd5eda680c4cf004f5dd314d
Gabor Benko <benkogabor87@gmail.com>
2024-03-01 15:51:46 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use std::io;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("Please input your progress");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("failed to read line");
+
+    println!("You guessed: {guess}")
+}
